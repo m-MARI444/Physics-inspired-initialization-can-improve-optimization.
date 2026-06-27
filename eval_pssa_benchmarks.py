f8262ac0841e11332255071acd84b368e99b1ebd
m-MARI444 <m-MARI444@users.noreply.github.com>
2026-06-27 22:44:26 +0530

    Fix missing tqdm import in eval_pssa_benchmarks.py

--- eval_pssa_benchmarks.py
+++ eval_pssa_benchmarks.py
@@ -10,6 +10,7 @@ from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
 from datasets import load_dataset
 from huggingface_hub import hf_hub_download
+from tqdm import tqdm
 
 # Import model definition dynamically from our local training module
 from kaggle_pssa_llm import PSSALanguageModel
